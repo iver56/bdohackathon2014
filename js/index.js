7ab84dd9de648053b8d70700fdbcf0aa0976ae35
iver56 <iver56@hotmail.com>
2014-03-02 01:52:00 +0100

    Remove help button. Implement add kvittering.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).on("change", "#kvittering-file-input", function () {
+    var now = new Date();
+    var day = ("0" + now.getDate()).slice(-2);
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+    var today = (day) + "." + (month) + "." + now.getFullYear();
+    $('#kvittering-dato').val(today);
+    $.mobile.navigate("#kvittering-input");
+});
+
+$(document).on("click", "#kvittering-lagre", function (e) {
+    var beskrivelse = $('#kvittering-beskrivelse').val(),
+        dato = $('#kvittering-dato').val(),
+        sum = $('#kvittering-sum').val(),
+        $anchor = $('<a><h3>' + beskrivelse + '</h3><p>' + dato + '</p><p>Sum: ' + sum + '</p></a>'),
+        $li = $('<li/>').append($anchor);
+    $('#kvitteringer-ul').append($li).listview('refresh');
+});
